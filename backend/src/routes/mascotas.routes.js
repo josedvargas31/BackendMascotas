516a1aed4fd5d7cb651d0efe41cb68eb241c4ba9
josedvargas31 <dajozavargas@gmail.com>
2024-09-07 21:37:16 -0500

    Implementando cambios necesarios para la actualizacion de la mascota

--- backend/src/routes/mascotas.routes.js
+++ backend/src/routes/mascotas.routes.js
@@ -3,19 +3,18 @@ import { listarMascotas, registrarMascota, actualizarMascota, eliminarMascota, b
 import upload from '../config/multer.config.js'; 
 import { uploadImage } from "../config/imagenes.controller.js";
 import { validarToken } from "../controllers/validacion.controller.js";
+import { validateActualizarMascota, validateCrearMascota } from "../validation/mascotas.validation.js";
 
 const MascotaRoutes = Router();
 
 MascotaRoutes.get("/listar", /*  validarToken,  */ listarMascotas);
-MascotaRoutes.post("/registrar",  validarToken,  upload.array('imagenes', 4), uploadImage, registrarMascota);
+MascotaRoutes.post("/registrar",  validarToken, /* validateCrearMascota, */ upload.array('imagenes', 4), registrarMascota);
 MascotaRoutes.get('/conteo/estado', validarToken, obtenerConteoPorEstado);
-MascotaRoutes.put("/actualizar/:id_mascota", /* validarToken, */  upload.array('imagenes', 4), uploadImage, actualizarMascota);
+MascotaRoutes.put("/actualizar/:id_mascota", validarToken, validateActualizarMascota,  upload.array('imagenes', 4), actualizarMascota);
 MascotaRoutes.delete("/eliminar/:id_mascota", validarToken, eliminarMascota);
 MascotaRoutes.get("/buscar/:id_mascota", validarToken, buscarMascota);
 MascotaRoutes.get('/pdf/:id', generarFichaTecnica);
 /*  */
-// MascotaRoutes.post('/iniciar/:id_mascota', iniciarAdopcion);
-// MascotaRoutes.post('/administrar/:id_adopcion', administrarAdopcion);
-// MascotaRoutes.get('/listarConUsuarios', listarMascotasConUsuarios);
+
 
 export default MascotaRoutes;
